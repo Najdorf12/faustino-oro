56e4ab1dbfb7072ef9f5cb69ef5e4bbb584bbb9c
Najdorf <agustin.morro@gmail.com>
2026-02-25 10:12:39 -0300

    add styles

--- src/components/landing/ui/layout/NavbarLanding.tsx
+++ src/components/landing/ui/layout/NavbarLanding.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
-import imgNav from "@/assets/images/icon1.png"
+import imgNav from "@/assets/images/icons/iconKnight.svg"
 import Image from "next/image";
 
 export default function NavbarLanding() {
   return (
-    <nav className="w-full absolute top-0 z-200 text-stone-300 px-4 pt-2 flex flex-row gap-4 md:gap-10 md:justify-start md:px-9 md:pt-4 lg:px-16 2xl:text-lg">
-      <div className="rounded-lg w-10 bg-sky-800 p-2 border-zinc-300 border z-100 lg:p-2 2xl:w-14">
+    <nav className="w-full absolute top-0 z-200 text-zinc-300 px-4 pt-2 flex flex-row gap-4 md:gap-10 md:justify-start md:px-9 md:pt-4 lg:px-16 2xl:text-lg">
+      <div className="rounded-lg w-10 bg-sky-800 p-2 border-zinc-300 border z-100 lg:p-2 lg:w-12 2xl:w-14">
         <Image
           src={imgNav}
           alt="Beautiful landscape for your journey"
           className="object-contain w-full"
         />
       </div>
-      <ul className="flex items-center justify-end gap-3 font-medium text-sm sm:text-base md:flex-row md:justify-start lg:gap-9 lg:text-lg">
+      <ul className="flex items-center justify-end gap-3 md:font-medium text-sm sm:text-base md:flex-row md:justify-start lg:gap-9 lg:text-lg">
         <li>
           <Link
             href="/"
