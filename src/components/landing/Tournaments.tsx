b1f75483a9a7f4183ff129eeaaa3bf62574fda4f
Najdorf <agustin.morro@gmail.com>
2026-03-04 17:36:27 -0300

    add styles

--- src/components/landing/Tournaments.tsx
+++ src/components/landing/Tournaments.tsx
@@ -13,21 +13,21 @@ export default function Tournaments({ data }: TournamentsProps) {
   return (
     <section
       id="tournaments"
-      className="w-full min-h-screen flex flex-col gap-12 relative justify-center items-center mt-9 pt-9 pb-12 bg-sky-900 md:h-screen lg:mt-12 lg:pt-12 lg:gap-16 xl:gap-18 lg:px-12 2xl:px-16 2xl:gap-20 3xl:mt-14"
+      className="w-full min-h-screen flex flex-col gap-12 relative justify-center items-center mt-9 pt-9 pb-12 bg-sky-900 md:h-screen lg:mt-12 lg:px-12 lg:pt-14 lg:gap-14 xl:gap-16 xl:pt-14 2xl:px-16 2xl:gap-20 3xl:mt-14"
     >
       <div className="min-h-screen w-full absolute inset-0 z-50"></div>
-      <article className="flex flex-col pl-4 gap-6 lg:flex-row lg:items-start  lg:px-9 lg:w-full ">
-        <h6 className="text-zinc-200 text-[2.6rem] sm:text-5xl font-medium lg:text-7xl lg:leading-12 2xl:text-[5rem] 2xl:leading-11 3xl:text-[5.5rem] ">
+      <article className="flex flex-col pl-4 gap-6 lg:flex-row lg:items-start lg:px-9 lg:w-full 2xl:gap-9 ">
+        <h6 className=" text-zinc-200 text-[2.6rem] sm:text-5xl font-medium lg:text-6xl lg:leading-14  xl:text-7xl xl:leading-16 2xl:text-[5rem] 2xl:leading-11 3xl:leading-20 3xl:text-[5.5rem]">
           Torneos
         </h6>
-        <p className="flex items-center text-zinc-300 max-w-130 text-start border-l-2 border-sky-600 text-sm font-medium pl-3 md:pl-6 py-1 md:leading-6 md:text-lg md:max-w-150 2xl:text-xl lg:py-2 ">
+        <p className="flex items-center text-zinc-300 max-w-130 text-start border-l-2 border-sky-600 text-sm font-medium pl-3 md:pl-6 py-1 md:leading-6 md:text-lg md:max-w-150 2xl:text-xl 2xl:leading-7 lg:py-2 xl:py-4 2xl:max-w-170 3xl:text-2xl 3xl:max-w-200 3xl:py-6">
           Consulta aquí la lista de próximos torneos en los que participará.
           Sigue su calendario y mantente al día con su increíble recorrido
           competitivo
         </p>
       </article>
 
-      <div className="w-full z-50 relative flex flex-wrap justify-center items-center gap-x-2 px-1 gap-y-5 max-w-150 sm:gap-x-4 lg:gap-12 lg:max-w-fit">
+      <div className="w-full z-50 relative flex flex-wrap justify-center items-center gap-x-2 px-1 gap-y-5 max-w-150 sm:gap-x-4 lg:gap-12 xl:gap-14 lg:max-w-fit">
         {data && data.length > 0 ? (
           data.map((tournament, i) => (
             <CardTournament
@@ -49,7 +49,7 @@ export default function Tournaments({ data }: TournamentsProps) {
           <p className="text-zinc-300">No hay torneos disponibles</p>
         )}
       </div>
-      <div className=" xl:mt-2 3xl:mt-3">
+      <div className="3xl:mt-2">
         <ButtonTournaments />
       </div>
       <div
