a6cb736e0b29e0730d55558eab3eb0c65e7230e6
Najdorf <agustin.morro@gmail.com>
2026-03-06 11:28:27 -0300

    fix tournaments historial

--- src/components/landing/Achievements.tsx
+++ src/components/landing/Achievements.tsx
@@ -10,18 +10,18 @@ interface AchievementsProps {
 export default function Achievements({ data }: AchievementsProps) {
   return (
     <>
-      <section className="w-full h-full mt-6 z-100 flex flex-col gap-10 items-center relative lg:px-12 lg:mt-10 2xl:px-16 ">
-        <article className="flex flex-col pl-4 gap-6 lg:flex-row lg:px-9 lg:w-full ">
-          <h6 className="text-zinc-200 text-[2.6rem] sm:text-5xl font-medium lg:text-7xl lg:leading-12 2xl:text-[5rem] 2xl:leading-11 3xl:text-[5.5rem] ">
+      <section className="w-full h-full mt-6 z-100 flex flex-col gap-10 items-center relative lg:px-12 lg:mt-12 2xl:px-16 ">
+        <article className="flex flex-col pl-4 gap-6 lg:flex-row lg:px-9 lg:w-full xl:gap-7 3xl:gap-9">
+          <h6 className="text-zinc-200 text-[2.6rem] sm:text-5xl font-medium lg:text-7xl lg:leading-12 xl:leading-14 2xl:text-[5rem] 2xl:leading-12 3xl:text-[5.5rem] 3xl:leading-17 ">
             Logros
           </h6>
-          <h6 className="flex items-center text-zinc-500 max-w-130 text-start border-l-2 border-sky-600 text-sm font-medium pl-3 md:pl-6 py-1 md:leading-6 md:text-lg 2xl:text-xl lg:py-2 ">
+          <h6 className="flex items-center text-zinc-500 max-w-130 text-start border-l-2 border-sky-600 text-sm font-medium pl-3 md:pl-6 py-1 md:leading-6 md:text-lg lg:py-2 xl:py-3 2xl:text-xl  3xl:text-2xl 3xl:max-w-150 3xl:leading-8 ">
             Con dedicación, esfuerzo y disciplina, Faustino sigue conquistando
             nuevos desafíos en cada partida.
           </h6>
         </article>
 
-        <section className="relative w-[96%] mx-auto flex flex-col px-4 py-6 lg:pl-12 lg:py-9 ">
+        <section className="relative w-[96%] mx-auto flex flex-col px-4 py-6 lg:pl-12 lg:pt-9  ">
           <div className="absolute inset-0 z-20 bg-linear-to-bl from-zinc-800/10 via-zinc-800/60 to-sky-900 flex justify-center items-center rounded-3xl"></div>
           <picture className="w-full h-full border-3 border-zinc-700 rounded-3xl absolute inset-0 mx-auto shadow-2xl shadow-sky-800">
             <source
@@ -56,7 +56,7 @@ export default function Achievements({ data }: AchievementsProps) {
           </ul>
         </section>
       </section>
-      <legend className="w-full justify-center relative text-center flex items-center gap-3 text-zinc-500 mt-14 text-sm sm:text-base md:mt-16 md:text-xl lg:mt-20 lg:pb-3 lg:gap-4 xl:gap-6 2xl:text-2xl 3xl:text-3xl 3xl:mt-24">
+      <legend className="w-full justify-center relative text-center flex items-center gap-3 text-zinc-500 mt-14 text-sm sm:text-base md:mt-16 md:text-xl lg:mt-18 lg:pb-3 lg:gap-4 xl:gap-6 2xl:text-2xl 3xl:text-3xl 3xl:mt-24">
         <div className="w-12 md:w-32 h-0.5 bg-sky-700 lg:w-36"></div>
         EL ÉXITO OCURRE CUANDO TUS SUEÑOS SON MAS GRANDES QUE TUS EXCUSAS
         <div className="w-12 md:w-32 h-0.5 bg-sky-700 lg:w-36"></div>
