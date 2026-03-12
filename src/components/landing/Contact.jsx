a15392384a7e4f1b8a6ad2e176d79f6b62aa3c1a
Najdorf <agustin.morro@gmail.com>
2026-03-12 17:14:01 -0300

    add styles

--- src/components/landing/Contact.jsx
+++ src/components/landing/Contact.jsx
@@ -48,11 +48,11 @@ export const Contact = () => {
         </div>
         <section className="relative h-full w-full pointer-events-none flex flex-col justify-center  md:flex-row md:justify-center items-center ">
           <article className="flex flex-col md:justify-start md:pt-12 md:w-[60%] md:h-full md:pl-9 lg:pt-20 md:gap-28 xl:gap-50 2xl:pl-14 3xl:pt-28 ">
-            <div className="text-center px-4 lg:text-start md:px-0">
-              <h5 className="text-[3.25rem]  sm:text-6xl lg:text-8xl 2xl:text-[7rem] 3xl:text-9xl font-medium text-zinc-200 z-30 relative">
-                CONTACTO
+            <div className="px-1  md:px-0">
+              <h5 className="text-center lg:text-start  text-[3.25rem]  sm:text-6xl lg:text-8xl 2xl:text-[7rem] 3xl:text-9xl font-medium text-zinc-200 z-30 relative">
+                Contacto
               </h5>
-              <p className="text-zinc-300 mt-6 text-start text-sm md:text-base max-w-145 border-l-2 py-1 pl-3 border-sky-600 lg:mt-6 font-medium lg:py-2 lg:text-lg 2xl:max-w-150">
+              <p className="text-zinc-300 mt-6 text-start text-sm md:text-base max-w-145 border-l-2 py-1 pl-3 border-sky-600 lg:mt-6 font-medium lg:py-2 lg:text-lg lg:ml-1 2xl:max-w-150 lg:pl-4">
                 Fausti quiere escucharte. Ponte en contacto para invitarlo a
                 eventos, colaboraciones o simplemente para alentarlo en su
                 carrera.
