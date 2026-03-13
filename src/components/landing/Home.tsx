591b12e4cc52377131044f473a22347b594fac4b
Najdorf <agustin.morro@gmail.com>
2026-03-13 11:21:39 -0300

    add styles

--- src/components/landing/Home.tsx
+++ src/components/landing/Home.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import imgHome from "@/assets/images/img5-removebg3.png";
-import imgHomeMobile from "@/assets/images/img5-removebgmobile.png";
+import imgHome from "@/assets/images/img5.webp";
+import imgHomeMobile from "@/assets/images/img5-3.jpg";
 import icon from "@/assets/images/icons/iconKnight.svg";
 
 export default function Home() {
   return (
     <>
       <section className="bg-radial from-20% from-zinc-50 to-stone-100 h-[95vh] md:h-screen w-full relative pb-2.5 lg:pb-1 ">
-        <div className="absolute inset-0 z-20  bg-zinc-800/45 lg:bg-zinc-800/20"></div>
+        <div className="absolute inset-0 z-20  bg-zinc-800/50 lg:bg-zinc-800/40"></div>
         <picture className="w-full h-full z-15 absolute inset-0 mx-auto ">
           <source media="(min-width: 1024px)" srcSet={imgHome.src} />
           <Image
@@ -33,13 +33,13 @@ export default function Home() {
               "radial-gradient(ellipse 60% 60% at 50% 50%, #000 30%, transparent 70%)",
           }}
         />
-
+{/*  bg-linear-to-t from-sky-800 via-gray-100 to-gray-300 lg:from-zinc-200 lg:via-zinc-300 lg:to-zinc-300 bg-clip-text text-transparent  */}
         <article className="relative z-100 w-full h-full flex flex-col items-center justify-end gap-1 md:gap-2  lg:w-1/2 lg:justify-center lg:pt-12">
-          <h1 className=" bg-linear-to-t from-sky-800 via-gray-100 to-gray-300 lg:from-sky-900 lg:via-gray-800 lg:to-sky-900 bg-clip-text text-transparent relative z-100 text-[2.8rem] leading-16 sm:text-[3rem] sm:leading-18 font-superlarky text-center md:text-[4rem] lg:leading-32 lg:text-[5.9rem] xl:leading-38 xl:text-[7.2rem] 2xl:leading-44 2xl:text-[8rem] 3xl:leading-50 3xl:text-[9.3rem]">
+          <h1 className="text-zinc-200 lg:text-zinc-200 relative z-100 text-[2.8rem] leading-16 sm:text-[3rem] sm:leading-18 font-superlarky text-center md:text-[4rem] lg:leading-32 lg:text-[5.9rem] xl:leading-38 xl:text-[7.2rem] 2xl:leading-44 2xl:text-[8rem] 3xl:leading-50 3xl:text-[9.3rem]">
             Faustino Oro
           </h1>
-          <p className="font-medium flex items-center gap-2 text-zinc-50  lg:text-zinc-500 text-base sm:text-[1.1rem] lg:text-xl xl:text-[1.30rem] 2xl:text-[1.5rem] 3xl:text-3xl">
-            <span className="border-2 border-zinc-300 text-zinc-50 lg:border-zinc-50 px-1 py-0.5 font-ubuntu rounded-sm lg:py- lg:px-1.5">
+          <p className="font-medium flex items-center gap-2 text-zinc-50  lg:text-zinc-300 text-base sm:text-[1.1rem] lg:text-xl xl:text-[1.30rem] 2xl:text-[1.5rem] 3xl:text-3xl">
+            <span className="border-2 border-zinc-300 text-zinc-300 lg:border-zinc-400 lg:text-zinc-300 px-1 py-0.5 font-ubuntu rounded-sm lg:py- lg:px-1.5">
               IM
             </span>{" "}
             MAESTRO INTERNACIONAL
--- src/components/landing/ui/layout/NavbarLanding.tsx
+++ src/components/landing/ui/layout/NavbarLanding.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function NavbarLanding() {
   return (
-    <nav className="w-full absolute top-0 z-200 text-zinc-50 lg:text-zinc-500 px-4 pt-2 flex flex-row gap-4 md:gap-10 md:justify-start md:px-6  md:pt-4 lg:px-6 xl:pt-6 xl:px-14 2xl:text-lg">
+    <nav className="w-full absolute top-0 z-200 text-zinc-100 lg:text-zinc-320 px-4 pt-2 flex flex-row gap-4 md:gap-10 md:justify-start md:px-6  md:pt-4 lg:px-6 xl:pt-6 xl:px-14 2xl:text-lg">
       <div className="rounded-lg w-10  bg-sky-800 p-1.5 border-zinc-300 border z-100 lg:p-2 lg:w-12 2xl:w-14">
         <Image
           src={imgNav}
@@ -19,7 +19,7 @@ export default function NavbarLanding() {
             className="flex justify-center transition-colors cursor-pointer "
           >
             <button id="cta">
-              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-700 duration-400">Inicio</span>
+              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Inicio</span>
             </button>
           </Link>
         </li>
@@ -30,7 +30,7 @@ export default function NavbarLanding() {
             className="flex justify-center transition-colors "
           >
              <button id="cta">
-              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-700 duration-400">Torneos</span>
+              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Torneos</span>
             </button>
           </Link>
         </li>
@@ -40,7 +40,7 @@ export default function NavbarLanding() {
             className="flex justify-center transition-colors "
           >
               <button id="cta">
-              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-700 duration-400">Noticias</span>
+              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Noticias</span>
             </button>
           </Link>
         </li>
@@ -50,7 +50,7 @@ export default function NavbarLanding() {
             className="flex justify-center transition-colors "
           >
               <button id="cta">
-              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-700 duration-400">Contacto</span>
+              <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Contacto</span>
             </button>
           </Link>
         </li>
