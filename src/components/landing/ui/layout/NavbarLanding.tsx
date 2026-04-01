ebe94674dce4af6ee1915131be5bf284e266213c
Najdorf <agustin.morro@gmail.com>
2026-04-01 13:19:57 -0300

    add styles

--- src/components/landing/ui/layout/NavbarLanding.tsx
+++ src/components/landing/ui/layout/NavbarLanding.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function NavbarLanding() {
   return (
-    <nav className="w-full absolute top-0 z-200 text-zinc-300 lg:text-zinc-320 px-4 pt-2 flex flex-row gap-4 md:gap-10 md:justify-start md:px-6  md:pt-4 lg:px-6 xl:pt-6 xl:px-14 2xl:text-lg">
+    <nav className="w-full absolute top-0 z-200 text-zinc-300 lg:text-zinc-320 px-4 pt-2 flex flex-row gap-3 md:gap-10 md:justify-start md:px-6  md:pt-4 lg:px-6 xl:pt-6 xl:px-14 2xl:text-lg">
       <div className="rounded-lg w-10  bg-sky-800 p-1.5 border-zinc-300 border z-100 lg:p-2 lg:w-12 2xl:w-14">
         <Link href="/">
           <Image
@@ -14,17 +14,17 @@ export default function NavbarLanding() {
           />
         </Link>
       </div>
-      <ul className="flex items-center justify-end gap-3 md:font-medium text-sm  md:flex-row md:justify-start font-medium lg:gap-7 lg:text-lg">
-        {/*      <li>
+      <ul className="flex items-center justify-end gap-3 md:font-medium text-sm  md:flex-row md:justify-start font-medium lg:gap-7 lg:text-lg 3xl:text-xl">
+            <li>
           <Link
             href="/"
-            className="flex justify-center transition-colors cursor-pointer "
+            className="hidden lg:flex justify-center transition-colors cursor-pointer "
           >
             <button id="cta">
               <span className="hover-underline-animation pb-1 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Inicio</span>
             </button>
           </Link>
-        </li> */}
+        </li> 
 
         <li>
           <Link
--- src/components/landing/ui/layout/NavbarPages.tsx
+++ src/components/landing/ui/layout/NavbarPages.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function NavbarPages() {
   return (
-    <nav className="w-full absolute top-0 z-200 text-zinc-200  px-4 pt-2 flex flex-row gap-4 md:gap-10 md:justify-start md:px-6  md:pt-4 lg:px-6 xl:pt-6 xl:px-14 2xl:text-lg">
+    <nav className="w-full absolute top-0 z-200 text-zinc-200  px-4 pt-2 flex flex-row gap-3 md:gap-6 md:justify-start md:px-6  md:pt-4 lg:px-6 xl:pt-6 xl:px-14 2xl:text-lg">
       <div className="rounded-lg w-10  bg-sky-800 p-1.5 border-zinc-300 border z-100 lg:p-2 lg:w-12 2xl:w-14">
         <Image
           src={imgNav}
@@ -12,14 +12,16 @@ export default function NavbarPages() {
           className="object-contain w-full"
         />
       </div>
-      <ul className="flex items-center justify-end gap-2.5 md:font-medium text-sm sm:gap-3  md:flex-row md:justify-start font-medium lg:gap-8 lg:text-lg">
+      <ul className="flex items-center justify-end gap-2.5 md:font-medium text-sm   md:flex-row md:justify-start font-medium lg:gap-6 lg:text-lg 3xl:text-xl">
         <li>
           <Link
             href="/"
-            className="flex justify-center transition-colors cursor-pointer "
+            className="hidden lg:flex justify-center transition-colors cursor-pointer "
           >
             <button id="cta">
-              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Inicio</span>
+              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">
+                Inicio
+              </span>
             </button>
           </Link>
         </li>
@@ -29,8 +31,10 @@ export default function NavbarPages() {
             href="/tournaments"
             className="flex justify-center transition-colors "
           >
-             <button id="cta">
-              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Torneos</span>
+            <button id="cta">
+              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">
+                Torneos
+              </span>
             </button>
           </Link>
         </li>
@@ -39,8 +43,22 @@ export default function NavbarPages() {
             href="/notices"
             className="flex justify-center transition-colors "
           >
-              <button id="cta">
-              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Noticias</span>
+            <button id="cta">
+              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">
+                Noticias
+              </span>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/#partners"
+            className="flex justify-center transition-colors "
+          >
+            <button id="cta">
+              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">
+                Partners
+              </span>
             </button>
           </Link>
         </li>
@@ -49,8 +67,10 @@ export default function NavbarPages() {
             href="/#contact"
             className="flex justify-center transition-colors "
           >
-              <button id="cta">
-              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">Contacto</span>
+            <button id="cta">
+              <span className="hover-underline-animation pb-1.5 px-0.5 lg:pb-2 lg:px-1 hover:text-sky-500 duration-400">
+                Contacto
+              </span>
             </button>
           </Link>
         </li>
